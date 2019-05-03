a42a64f72342d1d33c0b5ccbc0a28490f4080fa6
Michael Poulshock <mpoulshock@gmail.com>
2019-05-03 15:21:09 -0400

    Cleaner version of proof trees

--- akkadian/basic.py
+++ akkadian/basic.py
@@ -1,19 +1,6 @@
 from akkadian.Value import *
 
 
-# MEMOIZATION
-
-# def memoize(f):
-# #     memo = {}
-# #
-# #     def helper(*args):
-# #         if args not in memo:
-# #             memo[args] = f(*args)
-# #         return memo[args]
-# #
-# #     return helper
-
-
 # COMPARISON
 
 # Internal, static >= function
